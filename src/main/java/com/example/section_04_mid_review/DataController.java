3c5a852ea1cc57f7d84979b1880bb6ca87d3f816
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-17 00:49:07 +0600

    button works

--- src/main/java/com/example/section_04_mid_review/DataController.java
+++ src/main/java/com/example/section_04_mid_review/DataController.java
@@ -3,6 +3,9 @@ package com.example.section_04_mid_review;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class DataController {
 
     public TextField packageNameTextField;
@@ -12,13 +15,32 @@ public class DataController {
     public ComboBox <String> availabilityComboBox;
     public DatePicker offerEndsDatePIcker;
     public Label errorText;
-    public TableColumn PackageNameCol;
-    public TableColumn datAmountCol;
-    public TableColumn priceCol;
-    public TableColumn validityCol;
-    public TableColumn availabilityCol;
-    public TableColumn offerendsCol;
+    public TableColumn <DataPackage,String> PackageNameCol;
+    public TableColumn <DataPackage,Double> datAmountCol;
+    public TableColumn <DataPackage,Double> priceCol;
+    public TableColumn <DataPackage,String> validityCol;
+    public TableColumn <DataPackage,String> availabilityCol;
+    //public TableColumn <DataPackage, LocalDate> offerendsCol;
+    public TableColumn <DataPackage,LocalDate> offerEndsCol;
+    public TableView <DataPackage> dataPackageTableView;
+
+
+
+    public void initialize(){
+
+    }
+
+    ArrayList<DataPackage> dataPackageList = new ArrayList<>();
 
     public void handleCreatePackageButton(ActionEvent actionEvent) {
+        if(packageNameTextField.getText().isEmpty()){
+            errorText.setText("Plz Enter a package name!");
+            return;
+        }
+        if(dataAmountTextField.getText().isEmpty()){
+            errorText.setText("PLZ Enter a data amount");
+            return;
+        }
+        errorText.setText("DataPackage Saved!");
     }
 }
