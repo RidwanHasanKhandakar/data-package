0831b459657b4177e63eb35d204176bf236555a6
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-17 12:09:04 +0600

    Changed the title

--- src/main/java/com/example/section_04_mid_review/HelloApplication.java
+++ src/main/java/com/example/section_04_mid_review/HelloApplication.java
@@ -12,7 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("data.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Data Package!");
+
+        stage.setTitle("Data!");
         stage.setScene(scene);
         stage.show();
     }
